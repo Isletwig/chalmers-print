0b9eb49832cb1c50332ef5d17d0d0cd4579e467d
Isletwig <anton.oqvist@gmail.com>
2015-04-29 14:24:34 +0200

    New structure in the options and ability to print in grayscale and staple on torget

--- chalmers-print.sh
+++ chalmers-print.sh
@@ -13,6 +13,7 @@ PRINTER="$2"
 COPIES=1
 SIDES="y"
 PAGES="y"
+OPTIONS=""
 
 # checks if file is set, else prompt 
 if [[ -z "$FILENAME" ]]; then 
@@ -26,20 +27,10 @@ if [[ -z "$PRINTER" ]]; then
 	read PRINTER;
 fi
 
-# looks for nicknames
-if [[ "$PRINTER" = "torget" ]]; then
-	PRINTER="f-7207b-color1"
-elif [[ "$PRINTER" = "dd" ]]; then
-	PRINTER="ft-4011-laser3"
-elif [[ "$PRINTER" = "bulten" ]]; then
-	PRINTER="m-1117-laser2"	
-elif [[ "$PRINTER" = "fb" ]]; then
-	PRINTER="f-7105a-laser1"
-fi
-
 # number of copies
 printf "Number of copies: "
 read COPIES
+OPTIONS="-#$COPIES"
 
 # range of pages
 printf "Print all pages (y,n):"
@@ -48,6 +39,7 @@ read PAGES
 if [[ "$PAGES" = "n" ]]; then
 	printf "Choose range of pages (ex: 1-2 or 1-2,5): "
 	read PAGES
+	OPTIONS="$OPTIONS -o page-ranges=$PAGES"
 fi
 
 # promps for duplex
@@ -58,10 +50,42 @@ if [[ "$SIDES" = "n" ]]; then
 else
 	SIDES="two-sided-long-edge"
 fi
+OPTIONS="$OPTIONS -o sides=$SIDES"
+
+# looks for nicknames and askes for printer specific options
+if [ "$PRINTER" = "torget" ] || [ "$PRINTER" = "f-7207b-color1" ]; then
+	PRINTER="f-7207b-color1"
+	
+	printf "Color? (y,n): "
+	read COLOR
+
+	if [[ "$COLOR" = "n" ]]; then
+		COLOR="Gray"
+	else
+		COLOR="CMYK"
+	fi
+	OPTIONS="$OPTIONS -o ColorModel=$COLOR"
+
+	printf "Stapled? (y,n): "
+	read STAPLES
+
+	# staples put on up to the left
+	if [[ "$STAPLES" = "y" ]]; then
+		STAPLES="UpperLeft"
+		OPTIONS="$OPTIONS -o StapleLocation=$STAPLES"
+	fi
+
+elif [[ "$PRINTER" = "dd" ]]; then
+	PRINTER="ft-4011-laser3"
+elif [[ "$PRINTER" = "bulten" ]]; then
+	PRINTER="m-1117-laser2"	
+elif [[ "$PRINTER" = "fb" ]]; then
+	PRINTER="f-7105a-laser1"
+fi
 
 # promps for username
 printf "CID: "
 read CID
 
 # command for printing
-ssh $CID@remote11.chalmers.se lpr -#"$COPIES" -o "page-ranges=$PAGES" -o "sides=$SIDES" -P "$PRINTER" < "$FILENAME"
+ssh $CID@remote11.chalmers.se lpr "$OPTIONS" -P "$PRINTER" < "$FILENAME"
